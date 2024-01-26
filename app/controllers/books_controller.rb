a35c0c8842c1c17ade464388097c1b439e34b4a3
EC2 Default User <ec2-user@ip-172-31-34-130.ap-northeast-1.compute.internal>
2024-01-26 02:17:37 +0000

    [Update]show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
     @user = current_user
+    @book_new = Book.new
   end
 
   def index
@@ -44,5 +46,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
