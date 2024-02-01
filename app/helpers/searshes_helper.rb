302e50d07cd35b21beeec59c0e5987b6aef574ac
EC2 Default User <ec2-user@ip-172-31-34-130.ap-northeast-1.compute.internal>
2024-02-01 04:51:57 +0000

    [Update]View

--- app/helpers/searshes_helper.rb
+++
@@ -1,2 +0,0 @@
-module SearshesHelper
-end
--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class SearshesControllerTest < ActionDispatch::IntegrationTest
+class SearchesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
